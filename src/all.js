3fc0f741a65c407d509f98ce81c23e0841273e14
rose <rose_joh@163.com>
2019-05-22 17:45:37 +0800

    更新

--- src/all.js
+++ src/all.js
@@ -1,25 +1,3 @@
-import store from "./store";
 
 
-hadChange(e){
-  // 声明action
-  const action={
-    type："change_input_value";
-    value:e.taget.value
-  }
-  // 将action传递给store
-  store.dispatch(action)
-}
 
-// state只能接收数据不能操作数据，需要将数据进行深拷贝
-if(action.type === "change_input_value"){
-  const newState=JSON.parse(JSON.stringify(state));
-  newState.value=action.value;
-  return newState;
-}
-
-// 订阅store
-store.subScribe(this.hadChangeValue.bind(this))
-hadChangeValue(){
-  this.setState(store.getState())
-}
